70b9ca8ec5f9c67bc2ad7fc6c609946f06039973
erik-gustafsson <erikephggustafsson@gmail.com>
2023-03-07 10:59:08 +0100

    feat
    display a list of objects in main, OBS object generated on front end. Need to be converted to fetch backend stuff

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,6 +1,90 @@
-const Main = () =>{
+const Main = () => {
+  //TODO: replace this with data fetched from the server!
+  let tempHolder = [
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Btlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Ctlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+    {
+      projectName: "Atlas",
+      projectDescription: "bananas apple duck duck cow shoe train hello world!",
+    },
+  ];
+  
+  function Project(props) {
     return (
-        <h1>Main view</h1>
+      <div>
+        <h1>{props.project.projectName}</h1>
+        <p>{props.project.projectDescription}</p>
+      </div>
     );
-}
+  }
+
+  return (
+    <div>
+      <h1>Main view</h1>
+      <div className="collectionOfProjects">
+        {tempHolder.map(project =>(
+            <Project project = {project}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Main;
