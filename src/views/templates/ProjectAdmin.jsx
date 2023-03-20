498d7c1c08d4ee2b9a654b67b9d68c750ea37160
erik-gustafsson <erikephggustafsson@gmail.com>
2023-03-20 16:58:55 +0100

    feat
    display members

--- src/views/templates/ProjectAdmin.jsx
+++ src/views/templates/ProjectAdmin.jsx
@@ -1,85 +1,122 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../components/Template/TemplateStyle.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "../../components/Profile/Profile.css";
 import "../../components/Template/TemplateStyle.css";
+import "../../components/Project/ProjectStyle.css";
 import { Row, Col } from "react-bootstrap";
 import "../../components/Profile/Profile.css";
-import "../../components/Template/TemplateStyle.css";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import ProjectUtils from "../../components/Utils/ProjectUtils";
 
 const ProjectAdmin = (props) => {
-    const navigate = useNavigate();
-    const {projectId} = props;
+  const navigate = useNavigate();
+  const { projectId } = props;
 
-    const [data, setData] = useState({
-        projectName: "",
-        projectDescription: "",
+  const [data, setData] = useState({
+    projectName: "",
+    projectDescription: "",
+  });
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [showApplicants, setShowApplicants] = useState(false);
+
+  const displayEditForm = () => {
+    if (showEditForm) setShowEditForm(false);
+    else setShowEditForm(true);
+  };
+
+  const displayApplicants = () => {
+    if (showApplicants) setShowApplicants(false);
+    else setShowApplicants(true);
+  };
+
+  const hideEditForm = () => {
+    setShowEditForm(false);
+  };
+  const hideApplicants = () => {
+    setShowApplicants(false);
+  };
+
+  const { register, handleSubmit } = useForm();
+
+  const editProject = async (formData) => {
+    setShowEditForm(false);
+    axios
+      .put("https://localhost:7132/api/Projects/" + projectId, {
+        projectName: formData.projectName,
+        description: formData.projectDescription,
+        categoryName: formData.projectCategoryName,
+        isAvailable: formData.projectAvailability === "true",
+      })
+      .then(async () => {
+        const data = await ProjectUtils.getData(
+          `https://localhost:7132/api/Projects/${projectId}/AdminProjectView`
+        );
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error updating project:", error);
       });
-      const [showEditForm, setShowEditForm] = useState(false);
-    
-      const displayEditForm = () => {
-        if (showEditForm) setShowEditForm(false);
-        else setShowEditForm(true);
-      };
-      const hideEditForm = () => {
-        setShowEditForm(false);
-      };
+  };
 
-      const { register, handleSubmit } = useForm();
-      
-      const editProject = async (formData) => {
-        setShowEditForm(false);
-        axios
-          .put("https://localhost:7132/api/Projects/" + projectId, {
-            projectName: formData.projectName,
-            description: formData.projectDescription,
-            categoryName: formData.projectCategoryName,
-            isAvailable: formData.projectAvailability === "true",
-          })
-          .then(() => {
-            setData(ProjectUtils.getData(`https://localhost:7132/api/Projects/${projectId}/AdminProjectView`))
-          })
-          .catch((error) => {
-            console.error("Error updating element:", error);
-          });
-      };
 
-      const deleteProject = () => {
-        if (
-          window.confirm(
-            "Are you sure you wish to delete this project? This can't be undone!"
-          )
-        ) {
-          axios.delete("https://localhost:7132/api/Projects/" + projectId);
-          navigate("/main");
-        }
-      };
-     
-      useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await ProjectUtils.getData(
-              `https://localhost:7132/api/Projects/${projectId}/ProjectExist`
-              // `https://localhost:7132/api/Projects/${projectId}/AdminProjectView`
-            );
-            setData(data);
-          } catch {
-            setData(null);
-          }
-        };
-        fetchData();
-      }, [projectId]);
+  const deleteProject = () => {
+    if (
+      window.confirm(
+        "Are you sure you wish to delete this project? This can't be undone!"
+      )
+    ) {
+      axios.delete("https://localhost:7132/api/Projects/" + projectId);
+      navigate("/main");
+    }
+  };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await ProjectUtils.getData(
+          `https://localhost:7132/api/Projects/${projectId}/AdminProjectView`
+        );
+        setData(data);
+        console.log(data);
+      } catch {
+        setData(null);
+      }
+    };
+    fetchData();
+  }, [projectId]);
+
+  function MemberItem(props) {
+         //return the one item in a given format
+       return (
+        //  <Link to={`/project/${props.project.id}`} className="link">
+           <div className="bg-container rounded-10 px-2">
+             <h4> {props.member.userName}</h4>    
+           </div>
+        //  </Link>
+       );
+     }
+
+
+     function ApplicantItem(props) {
+        //return the one item in a given format
+      return (
+       //  <Link to={`/project/${props.project.id}`} className="link">
+          <div className="bg-white rounded-10 px-2">
+            <h5>{props.applicant.applicantName}</h5>
+            <p> {props.applicant.message} </p>    
+          </div>
+       //  </Link>
+      );
+    }
+
+  
   return (
     <div>
-        admin
       {data && (
         <div id="Content">
           {showEditForm && (
@@ -138,10 +175,50 @@ const ProjectAdmin = (props) => {
               </div>
             </div>
           )}
+
+            {showApplicants && (
+            <div>
+              <div className="dark" onClick={hideApplicants}></div>
+              <div className="aboveDark bg-container flex row">
+                <h4>Applicants:</h4>
+                <div style={{  height: "200px", overflow: "auto" }}>
+                  {data.applications &&
+                    data.applications.map((item, index) => (
+                      <ApplicantItem key={index} applicant={item} />
+                    ))}
+                </div>
+              </div>
+            </div>
+          )}
+
+
           <div id="PrimaryContent">
             <div className="py-0 my-0 px-2">
-              <h1>{data.projectName}</h1>
-              <p>{data.description}</p>
+              <div className="bg-container mt-2 px-3 pt-3 pb-1  rounded-10">
+                <h1>{data.projectName}</h1>
+                <p> (Project type: {data.categoryName})</p>
+              </div>
+              <div className="bg-container mt-2 px-1 rounded-10 description">
+                <p>{data.description}</p>
+              </div>
+              {data.repositoryLink && (
+                <div className="bg-container mt-2 p-1 rounded-10">
+                  <p> Repository: {data.repositoryLink}</p>
+                </div>
+              )}
+              <div className="bg-container mt-2 p-1 rounded-10">
+              {data.applications &&(
+                <p>Amount of unsorted applicants: {data.applications.length}</p>)}
+              </div>
+              <div className="bg-container mt-2 p-1 rounded-10 members">
+                <h3>Collaborators:</h3>
+                <div style={{  height: "200px", overflow: "auto" }}>
+                  {data.members &&
+                    data.members.map((item) => (
+                      <MemberItem key={item.id} member={item} />
+                    ))}
+                </div>
+              </div>
               <Link to="/Main">Go back to the main page</Link>
             </div>
           </div>
@@ -151,8 +228,11 @@ const ProjectAdmin = (props) => {
                 <button className="Button" onClick={displayEditForm}>
                   Edit Project
                 </button>
+                <button className="Button" onClick={displayEditForm}>
+                  Edit Project
+                </button>
                 <button className="Button mb-4" onClick={deleteProject}>
-                  Delete Project
+                  Edit Repository link
                 </button>
               </div>
             </div>
@@ -160,12 +240,12 @@ const ProjectAdmin = (props) => {
             <div className="bg-content m-3 p-2" id="SecondaryContent">
               <div className="w-100">
                 <button className="Button">Group Chat</button>
-                <button className="Button">Skills required</button>
+                <button className="Button" onClick={displayApplicants}>Display applicants</button>
               </div>
             </div>
           </div>
         </div>
-      )} 
+      )}
     </div>
   );
 };
